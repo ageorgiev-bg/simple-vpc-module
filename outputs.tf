16df0468a8f2a3ee651a01561b514808d0357d09
Alex Georgiev <sashsz@gmx.us>
2026-01-22 18:39:59 +0200

    cidr_block export

--- outputs.tf
+++ outputs.tf
@@ -2,3 +2,8 @@ output "vpc_id" {
   value       = module.vpc[*].vpc_id
   description = "Module VPC id."
 }
+
+output "cidr_block" {
+  value       = aws_vpc.main[*].cidr_block
+  description = "VPC cidr_block."
+}
