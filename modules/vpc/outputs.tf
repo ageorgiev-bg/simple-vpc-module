dce7d1e44868dd516de9ce56c9e0579f1b589f1d
Alex Georgiev <sashsz@gmx.us>
2026-01-23 19:21:27 +0200

    fix outputs

--- modules/vpc/outputs.tf
+++ modules/vpc/outputs.tf
@@ -1,10 +1,10 @@
 output "vpc_id" {
-  value       = aws_vpc.main[*].id
-  description = "Module VPC id."
+  value       = aws_vpc.main.id
+  description = "VPC id."
 }
 
 output "cidr_block" {
-  value       = aws_vpc.main[*].cidr_block
+  value       = aws_vpc.main.cidr_block
   description = "VPC cidr_block."
 }
 
@@ -14,6 +14,6 @@ output "public_subnets" {
 }
 
 output "private_subnets" {
-  value       = values(aws_subnet.public)[*].id # [for subnet in values(aws_subnet.private)[*] : subnet.id]
+  value       = values(aws_subnet.private)[*].id # [for subnet in values(aws_subnet.private)[*] : subnet.id]
   description = "VPC cidr_block."
 }
